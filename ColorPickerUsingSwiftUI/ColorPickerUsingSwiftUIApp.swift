0c795361d89d161c9e98647864c459a082dc8016
Tamim Dari Chowdhury <dari.tamim028@gmail.com>
2024-09-06 01:04:52 +0600

    [new-->] initial commit

---
+++ ColorPickerUsingSwiftUI/ColorPickerUsingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerUsingSwiftUIApp.swift
+//  ColorPickerUsingSwiftUI
+//
+//  Created by Tamim Dari Chowdhury on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerUsingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ColorPickerView()
+        }
+    }
+}
---
+++ ColorPickerUsingSwiftUI/ColorPickerView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  ColorPickerUsingSwiftUI
+//
+//  Created by Tamim Dari Chowdhury on 9/6/24.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    @State private var redColorValue: Double = 0.0
+    @State private var greenColorValue: Double = 0.0
+    @State private var blueColorValue: Double = 0.0
+    @State private var curColor: Color = Color(red: 0, green: 0, blue: 0)
+    
+    var body: some View {
+        VStack{
+            Text("Create a color and apply")
+                .padding(.bottom, 50)
+                .font(.system(size: 20, weight: .medium, design: .serif))
+                .foregroundStyle(.cyan)
+                
+            Circle()
+                .frame(width: 300, height: 300)
+                .foregroundStyle(curColor)
+                .padding(.bottom, 24)
+            HStack{
+                Text("Red")
+                    .frame(width: 60)
+                    .padding()
+                    .foregroundStyle(.red)
+                Slider(value: $redColorValue, in: 0...255)
+                    .padding()
+                    .tint(.red)
+            }
+            
+            HStack{
+                Text("Green")
+                    .frame(width: 60)
+                    .padding()
+                    .foregroundStyle(.green)
+                Slider(value: $greenColorValue, in: 0...255)
+                    .padding()
+                    .tint(.green)
+            }
+            HStack{
+                Text("Blue")
+                    .frame(width: 60)
+                    .padding()
+                    .foregroundStyle(.blue)
+                Slider(value: $blueColorValue, in: 0...255)
+                    .padding()
+                    .tint(.blue)
+            }
+            
+            Button("Apply Color"){
+                curColor = Color(red: redColorValue/255.0, green: greenColorValue/255.0, blue: blueColorValue/255.0)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(16)
+            
+            
+        }
+        
+    }
+}
+
+struct ColorPickerView_preview: PreviewProvider {
+    static var previews: some View {
+        ColorPickerView()
+    }
+}
