600aa138832aec6abd702f8d7629a2a1a1f37669
Tamim Dari Chowdhury <dari.tamim028@gmail.com>
2024-09-06 01:18:29 +0600

    [fix-->] When white color is applied, circle shape is not visible anymore issue fixed.

--- ColorPickerUsingSwiftUI/ColorPickerView.swift
+++ ColorPickerUsingSwiftUI/ColorPickerView.swift
@@ -13,14 +13,32 @@ struct ColorPickerView: View {
     @State private var blueColorValue: Double = 0.0
     @State private var curColor: Color = Color(red: 0, green: 0, blue: 0)
     
+    private let gradient = AngularGradient(
+        gradient: Gradient(colors: [Color.blue, .white]),
+        center: .center,
+        startAngle: .degrees(270),
+        endAngle: .degrees(0))
+    
     var body: some View {
         VStack{
             Text("Create a color and apply")
                 .padding(.bottom, 50)
                 .font(.system(size: 20, weight: .medium, design: .serif))
                 .foregroundStyle(.cyan)
-                
+            
             Circle()
+                .overlay(
+                    Circle()
+                        .strokeBorder(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.red, .blue, .green]), // Gradient colors
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            ),
+                            lineWidth: 1.0 // Border width
+                        )
+                        .opacity(0.099)
+                )
                 .frame(width: 300, height: 300)
                 .foregroundStyle(curColor)
                 .padding(.bottom, 24)
